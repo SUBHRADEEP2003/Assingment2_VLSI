a932cc109ff021596abae240ad29dd682312445f
SUBHRADEEP2003 <121381720+SUBHRADEEP2003@users.noreply.github.com>
2026-03-29 23:07:26 +0530

    Create TestBench.v

---
+++ TestBench.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+
+module tb_ripple_adder;
+
+    reg [3:0] A, B;
+    reg Cin;
+    wire [3:0] Sum;
+    wire Cout;
+
+    // Instantiate the DUT
+    ripple_carry_adder_4bit uut (
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Sum(Sum),
+        .Cout(Cout)
+    );
+
+    initial begin
+        // Monitor values
+        $monitor("Time=%0t | A=%b B=%b Cin=%b | Sum=%b Cout=%b",
+                  $time, A, B, Cin, Sum, Cout);
+
+        // Test cases
+        A=4'b0000; B=4'b0000; Cin=0; #10;
+        A=4'b0101; B=4'b0011; Cin=0; #10;
+        A=4'b1111; B=4'b0001; Cin=0; #10;
+        A=4'b1010; B=4'b0101; Cin=1; #10;
+        A=4'b1111; B=4'b1111; Cin=1; #10;
+
+        $finish;
+    end
+
+endmodule
